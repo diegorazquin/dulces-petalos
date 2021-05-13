72a988880b15c2cdaaa0981b97180768d36ed9e2
diego <diego.razquin@biko2.com>
2021-05-13 17:24:36 +0200

    refactor - reordena tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('Home', () => {
-  it('muestra una flor', () => {
-    render(<App/>);
-    expect(screen.getByText(/Rosa/i)).toBeInTheDocument();
-  });
   it('muestra el titulo de la tienda', () => {
     render(<App />);
     expect(screen.getByText(/Dulces pétalos/i)).toBeInTheDocument();
   })
+  it('muestra una flor', () => {
+    render(<App/>);
+    expect(screen.getByText(/Rosa/i)).toBeInTheDocument();
+  });
 })
