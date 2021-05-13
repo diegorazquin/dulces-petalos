b9371a423f30d269b4adace1cff23d2cf23ee73d
diego <diego.razquin@biko2.com>
2021-05-13 16:59:21 +0200

    test - muestra una flor
    Co-authored-by: marcosrgalvez <marcosrgalvez@users.noreply.github.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,9 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test('renders learn react link', () => {
-
-});
+describe('Home', () => {
+  it('muestra una flor', () => {
+    render(<App/>);
+    expect(screen.getByText(/Rosa/i)).toBeInTheDocument();
+  });
+})
